3e7ac8f5216bb7d85f90bd941df5d6dfb497685d
NHK1905 <nhk19598@gmail.com>
2023-03-22 03:18:19 +0700

    Update Header & Nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import './header.css'
+
+function Header() {
+
+    useEffect(() => {
+        const text = document.querySelector("header h2")
+        text.innerHTML = text.innerHTML
+        .split('')
+        .map((letter, idx) => `<p style='--x:${idx}'>${letter}</p>`)
+        .join('')
+    }, [])
+
+  return (
+    <header>
+        <div className="text">
+            <h1>My</h1>
+            <span>Portfolio</span>
+        </div>
+        <h2>creative-ambition</h2>
+    </header>
+  )
+}
+
+export default Header
